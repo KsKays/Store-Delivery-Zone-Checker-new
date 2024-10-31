3df394434c17a212a419d38920216273b5e0cfba
KKKKK <160151349+KsKays@users.noreply.github.com>
2024-10-31 19:29:50 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT;
 const frontend_url = process.env.FRONTEND_URL;
 
 const corsOption = {
-  origin: "http://localhost:5173",
+  origin: frontend_url,
 };
 
 //Dev Mode
